9c04408b897bf8c53bba632dfac1f1456f984ec1
silgue <alyssonsrodrigues@gmail.com>
2020-07-21 10:13:14 -0300

    criação aba documentos

--- js/prototipo.js
+++ js/prototipo.js
@@ -9,11 +9,20 @@ var docentesComponente = [];
 var listaEntidades = [];
 var entidadesSelecionadas = [];
 var entidadesParticipes = [];
+var tiposDocumentos = ['ATA APROVAÇÃO DEPARTAMENTO', 'AUTORIZAÇÃO PARTICIPAÇÃO EM PESQUISA', 'LIMITE DE CARGA HORÁRIA/REMUNERAÇÃO', 'HOMOLOGAÇÃO PROPESQ', 'HOMOLOGAÇÃO PROPLAN', 'HOMOLOGAÇÃO PPG'];
+var arquivosProjeto = [];
 
 var ufrn = new EntidadeParticipe('Contratante','UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE','24.823.767/0001-89','Avenida Salgado Filho,3000','Natal', 'RN');
 var funpec = new EntidadeParticipe('Contratada','Fundação Norte-Rio-Grandense de Pesquisa e Cultura','76.824.797/0001-03','Avenida Salgado Filho,3000','Natal', 'RN');
 
 entidadesParticipes.push(ufrn,funpec);
+
+function ArquivoProjeto(tipo,nome,descricao, arquivo){
+	this.tipo = tipo;
+	this.nome = nome;
+	this.descricao = descricao;
+	this.arquivo = arquivo;
+}
 
 function EntidadeParticipe(tipo,nome,cnpj, endereco, cidade, uf){
 	this.tipo = tipo;
@@ -93,7 +102,7 @@ function limpaSelect(elemento){
 
 function populaSelect(elemento, array){
 	array.forEach((opt) => {
-      option = new Option(opt, opt.toLowerCase());
+      option = new Option(opt, opt);
       elemento.options[elemento.options.length] = option;
     });
 }
@@ -112,6 +121,121 @@ function calcular(vlr1,vlr2,resultado) {
               var n1 = parseInt(document.getElementById('vlr1').value, 10);
               var n2 = parseInt(document.getElementById('vlr2').value, 10);
               document.getElementById('resultado').innerHTML = n1 + n2;
+}
+
+function inserirTipoDocumento(){
+	tiposDocumentos.push(document.getElementById('nome-tipo-documento').value);
+	limpaSelect(document.getElementById('select-tipos-documentos'));
+	populaSelect(document.getElementById('select-tipos-documentos'), tiposDocumentos);
+	populaTiposDocumento();
+}
+
+function populaTiposDocumento(){
+
+	var tabela = document.getElementById('tabela-tipos-documentos');
+
+	while(tabela.rows.length >2){
+		tabela.deleteRow(length-1);
+	}
+
+	tiposDocumentos.forEach(function (tipo) {
+		
+		// Captura a quantidade de linhas já existentes na tabela
+	   	var numLinhas = tabela.rows.length;
+	   	// Captura a quantidade de colunas da última linha da tabela
+	   	var numColunas = tabela.rows[numLinhas-1].cells.length;
+
+   		var novaLinha = tabela.insertRow(numLinhas);
+   		novaLinha.setAttribute("id",numLinhas+1);
+
+   		for (var j = 0; j < numColunas; j++) {
+	      var a, i;
+	       // Insere uma coluna na nova linha 
+	      novaCelula = novaLinha.insertCell(j);
+	      if(j===0){
+	      novaCelula.innerHTML = tipo;
+	      }else{
+	      	if(tipo != 'ATA APROVAÇÃO DEPARTAMENTO' && tipo !=  'AUTORIZAÇÃO PARTICIPAÇÃO EM PESQUISA' && tipo != 'LIMITE DE CARGA HORÁRIA/REMUNERAÇÃO' && tipo != 'HOMOLOGAÇÃO PROPESQ' && tipo != 'HOMOLOGAÇÃO PROPLAN' && tipo != 'HOMOLOGAÇÃO PPG'){
+	      		a = document.createElement("button");
+	        a.setAttribute("onclick","removerLinhaTabela(this.parentNode.parentNode.rowIndex,'tabela-tipos-documentos')");
+	        a.setAttribute("class", "btn btn-link");
+	        i = document.createElement("i");
+	        i.setAttribute("class", "far fa-trash-alt");
+	        a.appendChild(i);
+	        novaCelula.appendChild(a);
+	    	}
+	      }
+	   }
+	});
+
+}
+
+function inserirArquivo(){
+
+	var tipo = document.getElementById('select-tipos-documentos').value;
+	var nome = document.getElementById('nome-arquivo').value;
+	var descricao = document.getElementById('descricao-arquivo').value;
+	var arquivo = document.getElementById('arquivo-projeto').value;
+
+	var arquivoProjeto = new ArquivoProjeto(tipo,nome,descricao,arquivo);
+
+	arquivosProjeto.push(arquivoProjeto);
+
+	populaTabelaArquivos();
+
+}
+
+function populaTabelaArquivos(){
+
+	var tabela = document.getElementById('tabela-arquivos');
+
+	while(tabela.rows.length >2){
+		tabela.deleteRow(length-1);
+	}
+
+	arquivosProjeto.forEach(function (arquivo) {
+		
+		// Captura a quantidade de linhas já existentes na tabela
+	   	var numLinhas = tabela.rows.length;
+	   	// Captura a quantidade de colunas da última linha da tabela
+	   	var numColunas = tabela.rows[numLinhas-1].cells.length;
+
+   		var novaLinha = tabela.insertRow(numLinhas);
+   		novaLinha.setAttribute("id",numLinhas+1);
+
+   		for (var j = 0; j < numColunas; j++) {
+	      var a, i;
+	       // Insere uma coluna na nova linha 
+	      novaCelula = novaLinha.insertCell(j);
+	      if(j===0){
+	      	novaCelula.innerHTML = arquivo.tipo;
+	      }else if(j===1){
+	      	novaCelula.innerHTML = arquivo.nome;
+	      }else if(j===2){
+	      	novaCelula.innerHTML = arquivo.descricao;
+	      }else if(j===3){
+	      	novaCelula.innerHTML = arquivo.arquivo;
+	      }else{
+	      	a = document.createElement("button");
+	        a.setAttribute("onclick","removerLinhaTabelaArquivo(this.parentNode.parentNode.rowIndex,'tabela-arquivos','"+arquivo.nome+"')");
+	        a.setAttribute("class", "btn btn-link");
+	        i = document.createElement("i");
+	        i.setAttribute("class", "far fa-trash-alt");
+	        a.appendChild(i);
+	        novaCelula.appendChild(a);
+	      }
+	   }
+	});
+}
+
+function removerLinhaTabelaArquivo(i,idTabela,nomeArquivo){
+	arquivosProjeto.forEach(function(arquivo){
+		if(arquivo.nome === nomeArquivo){
+			arquivosProjeto.splice(arquivosProjeto.indexOf(arquivo), 1);
+		}
+	});
+	
+    document.getElementById(idTabela).deleteRow(i);
 }
 
 function inserirLinhaTabela(idTabela) {
